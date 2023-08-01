f5006299968d5c123bbb611b2e7dace977680dfb
ginofrancocba <ginofrancocba@gmail.com>
2023-08-01 00:19:55 -0300

    delete App

--- proyecto-semana-2/src/main.jsx
+++ proyecto-semana-2/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import Header from './Header.jsx'
 import Principal from './Principal.jsx'
 import Footer from './Footer.jsx'
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <Principal/>
-    <Footer />
-    
+    <Footer />  
   </React.StrictMode>,
 )
