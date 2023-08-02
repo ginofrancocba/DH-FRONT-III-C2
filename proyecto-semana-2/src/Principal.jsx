88a8f9d56aeec2b01b1af726f8b9550473b5dc5d
ginofrancocba <ginofrancocba@gmail.com>
2023-08-02 16:29:44 -0300

    Create components

--- proyecto-semana-2/src/Principal.jsx
+++ proyecto-semana-2/src/Principal.jsx
@@ -12,18 +12,18 @@ function Principal() {
 export default Principal;
 
 
-/*Front End III
-Clase 3: Práctica integradora
+/*Clase 3: Práctica integradora
 
-¡Hora de practicar! En esta actividad crearás un nuevo proyecto React utilizando la herramienta ViteJS.
+¡Hora de practicar! En esta actividad crearás un nuevo proyecto React utilizando
+ la herramienta ViteJS.
 Después de crear el proyecto deberás hacer una limpieza de los archivos no utilizados,
  y cuando haya terminado con la limpieza, tendrás que añadir algunos componentes JSX más 
  al contenido del archivo App.js,
-Simular una estructura con header, principal y footer. Header y footer pueden tener cualquier
- texto o elemento descriptivo (no es necesario que tenga un contenido realista) ej.
-  “este es el header”.
-Simularemos el inicio de un e-commerce. Dentro del main se renderizan productos cuyo nombre y 
-precio serán pasados como props.  Deben renderizar al menos 3 productos.
+Simular una estructura con header, principal y footer. 
+Header y footer pueden tener cualquier texto o elemento descriptivo 
+(no es necesario que tenga un contenido realista) ej. “este es el header”.
+Simularemos el inicio de un e-commerce. Dentro del main se renderizan productos 
+cuyo nombre y precio serán pasados como props.  Deben renderizar al menos 3 productos.
 Observaciones:
 En el Playground encontrarás información detallada sobre el uso de JSX y la creación de un
  proyecto React con ViteJS.
